7d773b2904339752d8e1634d7a7e76942ae81485
sophiecode1105 <sophiecode1105@gmail.com>
2022-05-16 17:22:02 +0900

    init: 초기셋팅 리덕스 툴킷 설치, 리액트 타입스크립트 설치, 스타일컴포넌트 설치

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import GlobalStyles from './GlobalStyles';
+import Main from './page/main';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Main />;
+    </>
+  );
+};
+
+export default App;
---
+++ src/page/main.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+const Main = () => {
+  return <Container></Container>;
+};
+export default Main;
